639bd2bca70f4c87f53e414b790af241c51615f8
DylanTartarini1996 <tartarinidylan@gmail.com>
2026-04-08 00:09:28 +0200

    :sparkles: basic ingestion functionalities

--- dyresearch/entities/knowledge_chunk.py
+++ dyresearch/entities/knowledge_chunk.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, Text, String, create_engine, text
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, Integer, Text, String
+from sqlalchemy.dialects.postgresql import JSONB
 from pgvector.sqlalchemy import Vector
 
 from dyresearch.entities import Base
@@ -9,14 +9,16 @@ class KnowledgeChunk(Base):
     __tablename__ = "knowledge_chunks"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
+    source_title = Column(String, nullable=False)
+    chunk_id = Column(Integer, nullable=False)
     text = Column(Text, nullable=False)
-    source_title = Column(String, nullable=True)
     subject = Column(String, index=True, nullable=False, default="General")
     source_type = Column(String, nullable=True)  # 'book', 'website', 'paper'
     authors = Column(String, nullable=True)
     page_or_url = Column(String, nullable=True)
     embedding = Column(Vector) # NOTE this might vary by embedding model chosen
     embedding_model = Column(String, nullable=True)
+    metadatas = Column(JSONB, nullable=True)
     
     def __repr__(self):
-        return f"<KnowledgeChunk(source='{self.source_title}', authors='{self.authors}')>"
+        return f"<KnowledgeChunk(source='{self.source_title}', authors='{self.authors}'"
