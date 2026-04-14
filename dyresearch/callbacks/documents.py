7cca9c972b18c7200abf9a8ed7067ada5ca83ccd
DylanTartarini1996 <tartarinidylan@gmail.com>
2026-04-14 22:51:40 +0200

    :bug: fix callback for upload

--- dyresearch/callbacks/documents.py
+++ dyresearch/callbacks/documents.py
@@ -3,7 +3,7 @@ from typing import Any, Dict
 from google.adk.tools import BaseTool
 from google.adk.tools.tool_context import ToolContext
 
-from ..tools.knowledge_base.vector_store import delete_source, ingest_source_chunks, ingest_source_file
+from ..tools.knowledge_base.vector_store import ingest_source_file
 from ..utils.logger import get_logger
 
 
@@ -15,7 +15,7 @@ async def upload_file_callback(
     args: Dict[str, Any], 
     tool_response: str,
     tool_context: ToolContext
-    ):
+):
     """ 
     Programmatic Hook: Automatically uploads a file obtained from the  
     `research_agent` inside the vector store. 
@@ -26,20 +26,22 @@ async def upload_file_callback(
         try: 
             url = args.get("url", None)
             filename = args.get("filename", None)
-            file_path = tool_response.split("✅ File successfully downloaded to:", 1)[1]
+            
+            file_path = tool_response.split("✅ File successfully downloaded to:", 1)[1].strip()
+            
             logger.info(f"⬆ Uploading {file_path} from callback..")
 
             ingestion_msg = await ingest_source_file(
                 file_path=file_path, 
                 title=filename, 
                 source_type="web_download", 
-                metadatas={"url": url}
+                metadata_json=str({"url": url})
             )
             
             logger.info(ingestion_msg)
         
         except Exception as e:
-            logger.warning(str(e))
+            logger.warning(f"Ingestion failed: {str(e)}")
 
     
         
